1658af5a14c6f704b379711b5d9c642fd0283384
alonso14ss <reiyan92@gmail.com>
2020-03-18 16:03:03 +0900

    가격 데이터 출력  - 1만이상 : 억  - 이하 : ,만 붙여서

--- app/src/main/java/co/kr/tjoeun/helloworld/a20200318_04_listview02/Adapters/RoomAdapter.java
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200318_04_listview02/Adapters/RoomAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -37,6 +38,31 @@ public class RoomAdapter extends ArrayAdapter<Room> {
         if (row == null) {
             row = inf.inflate(R.layout.room_list_item, null);
         }
+
+        Room data = mList.get(position);
+
+        TextView priceTxt = row.findViewById(R.id.priceTxt);
+        TextView addressAndFloorTxt = row.findViewById(R.id.addressAndFloorTxt);
+        TextView descTxt = row.findViewById(R.id.descTxt);
+
+//        가격 설정 =>setText에는 int값을 넣지 말자!
+//        1만이상? 억단위, 아니면? 숫자만 , 찍어서
+        if(data.getPrice()>=10000){
+//            ?억 ?천만 로 가공
+            int uk = data.getPrice() / 10000;
+            int thousand = data.getPrice() % 10000;
+
+            priceTxt.setText(String.format("%d억 %,d",uk,thousand));
+
+        }
+        else{
+            priceTxt.setText(String.format("%,d", data.getPrice()));
+        }
+
+
+
+
+
         return row;
     }
 }
